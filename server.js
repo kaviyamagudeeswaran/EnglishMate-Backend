2480efa55a09ec70c615454c2efd69a0d74fa289
kaviyamagudeeswaran <kaviya.022@onetoinfinity.org>
2025-12-02 10:29:15 +0530

    Fix server.js, authRoutes, and middleware for PathError & CORS issues

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ const PORT = process.env.PORT || 5000;
 // CORS CONFIG
 // ----------------------
 const allowedOrigins = [
-  "http://localhost:3000",
-  "http://localhost:4173",
-  "http://localhost:5173",
+  // "http://localhost:3000",
+  // "http://localhost:4173",
+  // "http://localhost:5173",
   "https://englishmate-frontend.onrender.com",
 ];
 
